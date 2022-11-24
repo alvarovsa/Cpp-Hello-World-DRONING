c3f908ce3ec289ea18fe655843a03b79a1b7dc18
allisonaguilar <allison.aguilar@uc.cl>
2022-11-24 19:36:32 +0100

    Hola Allison Aguilar

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 int main() {
     std::cout << "Hello World!";
-    printf("\n Hello I am Francisco Ramos Perez"); 
+    printf("\n Hello I am Francisco Ramos Perez");
+    printf("\n Hello I am Allison Aguilar Moya"); 
     return 0;
 }
 
